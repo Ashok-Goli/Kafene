6e5c491f6cc0f826df9a2c05f6037faf364b691a
Ashok-Goli <142201695+Ashok-Goli@users.noreply.github.com>
2023-09-25 17:28:23 +0530

    Update Order List Page.js

--- Order List Page.js
+++ Order List Page.js
@@ -8,123 +8,123 @@ $(document).ready(function () {
         localStorage.setItem('userLoginStatus', false)
         location.assign('./index.html')
     }
-    var responseArr;
-    $.get("https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/orders",
-        function (data) {
-            responseArr = data;
-            data.map((item) => {
-                createRows(item)
-                $('#count').html(data.length);
-            })
-        },
-    );
-    function createRows(data) {
-        let tr = (`
-        <tr class="table-row">
-            <td class="secondary-text">${data.id}</td>
-            <td class="primary-text">${data.customerName}</td>
-            <td class="primary-text">${data.orderDate}<br><span class="secondary-text">${data.orderTime}</span></td>
-            <td class="secondary-text">$${data.amount}</td>
-            <td class="primary-text">${data.orderStatus}</td>
-        </tr>`)
-        $('#table-body').append(tr);
-    }
-
-
-    var newCheckBox = document.getElementById('newCheckBox');
-    newCheckBox.addEventListener('change', function (e) {
-        e.preventDefault();
-        let tablebody = document.getElementById('table-body');
-        let tr = tablebody.getElementsByTagName('tr');
-        for (let i = 0; i < tr.length; i++) {
-            let td = tr[i].getElementsByTagName('td')[4];
-            if (td) {
-                let textValue = td.textContent || td.innerHTML;
-                if (textValue === 'New'){
-                    if(this.checked === true){
-                        tr[i].style.display = "";
-                        $('#count').html(parseInt($('#count').html()) + 1 );
-                    }else{
-                        tr[i].style.display = "none";
-                        $('#count').html(parseInt($('#count').html()) - 1 );
-                    }     
-                }
-            }
-        }
-        console.log(tablebody.getElementsByTagName('tr').length)
-    })
-
-
-
-    var DeliveredCheckBox = document.getElementById('DeliveredCheckBox');
-    DeliveredCheckBox.addEventListener('change', function (e) {
-        e.preventDefault();
-        let tablebody = document.getElementById('table-body');
-        let tr = tablebody.getElementsByTagName('tr');
-        for (let i = 0; i < tr.length; i++) {
-            let td = tr[i].getElementsByTagName('td')[4];
-            if (td) {
-                let textValue = td.textContent || td.innerHTML;
-                if (textValue === 'Delivered'){
-                    if(this.checked === true){
-                        tr[i].style.display = "";
-                        $('#count').html(parseInt($('#count').html()) + 1 );
-                    }else{
-                        tr[i].style.display = "none";
-                        $('#count').html(parseInt($('#count').html()) - 1 );
-                    }     
-                }
-            }
-        }
-    })
-
-
-
-
-    var IntransitcheckBox = document.getElementById('IntransitcheckBox');
-    IntransitcheckBox.addEventListener('change', function (e) {
-        e.preventDefault();
-        let tablebody = document.getElementById('table-body');
-        let tr = tablebody.getElementsByTagName('tr');
-        for (let i = 0; i < tr.length; i++) {
-            let td = tr[i].getElementsByTagName('td')[4];
-            if (td) {
-                let textValue = td.textContent || td.innerHTML;
-                if (textValue === 'InTransit'){
-                    if(this.checked === true){
-                        tr[i].style.display = "";
-                        $('#count').html(parseInt($('#count').html()) + 1 );
-                    }else{
-                        tr[i].style.display = "none";
-                        $('#count').html(parseInt($('#count').html()) - 1 );
-                    }     
-                }
-            }
-        }
-    })
-
-
-
-
-    var PackedCheckBox = document.getElementById('PackedCheckBox');
-    PackedCheckBox.addEventListener('change', function (e) {
-        e.preventDefault();
-        let tablebody = document.getElementById('table-body');
-        let tr = tablebody.getElementsByTagName('tr');
-        for (let i = 0; i < tr.length; i++) {
-            let td = tr[i].getElementsByTagName('td')[4];
-            if (td) {
-                let textValue = td.textContent || td.innerHTML;
-                if (textValue === 'Packed'){
-                    if(this.checked === true){
-                        tr[i].style.display = "";
-                        $('#count').html(parseInt($('#count').html()) + 1 );
-                    }else{
-                        tr[i].style.display = "none";
-                        $('#count').html(parseInt($('#count').html()) - 1 );
-                    }     
-                }
-            }
-        }
-    })
 });
+
+var tBody = document.getElementById("table-body")
+var count = document.getElementById("count")
+var updatedCount = count.innerText
+console.log(updatedCount)
+$.get("https://5fc1a1c9cb4d020016fe6b07.mockapi.io/api/v1/orders", function (response) {
+        var data = response
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+            tBody.innerHTML += `
+        <tr class="table-row" id="${data[i].orderStatus + i}">
+                                    <td class="secondary-text">${data[i].id}</td>
+                                    <td class="primary-text">${data[i].customerName}</td>
+                                    <td class="primary-text">${data[i].orderDate} <br><span class="secondary-text">${data[i].orderTime}</span></td>
+                                    <td class="secondary-text">$${data[i].amount}</td>
+                                    <td class="primary-text">${data[i].orderStatus}</td>
+                                </tr>`
+        }
+    })
+
+
+    var status1 = true
+    var status2 = true
+    var status3 = true
+    var status4 = true
+
+    function clicked(status) {
+        console.log(status)
+        if (status == "New") {
+            if (status1 == true) {
+                var element1 = document.getElementById(status + 1)
+                var element2 = document.getElementById(status + 2)
+                var element3 = document.getElementById(status + 8)
+                element1.style.display = "none"
+                element2.style.display = "none"
+                element3.style.display = "none"
+                status1 = false
+                count.innerText = count.innerText - 3
+                updatedCount = count.innerText
+            } else {
+                var element1 = document.getElementById(status + 1)
+                var element2 = document.getElementById(status + 2)
+                var element3 = document.getElementById(status + 8)
+                element1.style.display = "table-row"
+                element2.style.display = "table-row"
+                element3.style.display = "table-row"
+                status1 = true
+                count.innerText = parseInt(updatedCount) + 3
+                updatedCount = count.innerText
+
+            }
+        }
+        if (status == "Delivered") {
+            if (status2 == true) {
+                var element4 = document.getElementById(status + 0)
+                var element5 = document.getElementById(status + 3)
+                var element6 = document.getElementById(status + 4)
+                element4.style.display = "none"
+                element5.style.display = "none"
+                element6.style.display = "none"
+                status2 = false
+                count.innerText = count.innerText - 3
+                updatedCount = count.innerText
+            } else {
+                var element4 = document.getElementById(status + 0)
+                var element5 = document.getElementById(status + 3)
+                var element6 = document.getElementById(status + 4)
+                element4.style.display = "table-row"
+                element5.style.display = "table-row"
+                element6.style.display = "table-row"
+                status2 = true
+                count.innerText = parseInt(updatedCount) + 3
+                updatedCount = count.innerText
+            }
+        }
+        if (status == "Packed") {
+            if (status3 == true) {
+                var element7 = document.getElementById(status + 6)
+                var element8 = document.getElementById(status + 9)
+                element7.style.display = "none"
+                element8.style.display = "none"
+                status3 = false
+                console.log(status3)
+                count.innerText = count.innerText - 2
+                updatedCount = count.innerText
+            } else {
+                var element7 = document.getElementById(status + 6)
+                var element8 = document.getElementById(status + 9)
+                element7.style.display = "table-row"
+                element8.style.display = "table-row"
+                status3 = true
+                count.innerText = parseInt(updatedCount) + 2
+                updatedCount = count.innerText
+            }
+        }
+        if (status == "InTransit") {
+            if (status4 == true) {
+                var element9 = document.getElementById(status + 5)
+                var element10 = document.getElementById(status + 7)
+                var element11 = document.getElementById(status + 10)
+                element9.style.display = "none"
+                element10.style.display = "none"
+                element11.style.display = "none"
+                status4 = false
+                count.innerText = count.innerText - 3
+                updatedCount = count.innerText
+            } else {
+                var element9 = document.getElementById(status + 5)
+                var element10 = document.getElementById(status + 7)
+                var element11 = document.getElementById(status + 10)
+                element9.style.display = "table-row"
+                element10.style.display = "table-row"
+                element11.style.display = "table-row"
+                status4 = true
+                count.innerText = parseInt(updatedCount) + 3
+                updatedCount = count.innerText
+            }
+        }
+    }
